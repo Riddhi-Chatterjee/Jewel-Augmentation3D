7d7c39e0aa6ca1e2eaef7b49efeddf1db8e89e5c
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-09-05 20:06:39 +0530

    major updates

--- faceMesh.js
+++ faceMesh.js
@@ -7,8 +7,7 @@ const controlsElement2 = document.getElementsByClassName('control2')[0];
 //create a scene
 const scene = new THREE.Scene();
 
-//create a camera
-
+//width & height calculations:
 const visibleHeightAtZDepth = ( depth, camera ) => {
   // compensate for cameras not positioned at z=0
   const cameraOffset = camera.position.z;
@@ -27,6 +26,7 @@ const visibleWidthAtZDepth = ( depth, camera ) => {
   return height * camera.aspect;
 };
 
+//create a camera
 const cam = new THREE.PerspectiveCamera(
   90,
   window.innerWidth / window.innerHeight,
@@ -36,14 +36,83 @@ const cam = new THREE.PerspectiveCamera(
 cam.position.z = 190;
 
 //create the nose ring
-const geometry = new THREE.BoxGeometry(10, 10, 10);
-var material = new THREE.MeshBasicMaterial({
-  color: 0xfffff,
-  wireframe: false,
-});
-var nose_ring = new THREE.Mesh(geometry, material);
+class NoseRing
+{
+    constructor()
+    {
+        this.entity = new THREE.Object3D(); //Only this is to be added to the scene
 
-//add some texture to the nose ring ...TODO
+        this.stone_length = 5;
+        this.stone_breadth = 5;
+        this.stone_depth = 2;
+        this.stone_texture = new THREE.TextureLoader().load('./textures/ruby.jpg');
+        this.stone_material = new THREE.MeshBasicMaterial({map: this.stone_texture});
+        this.stone_geometry = new THREE.BoxGeometry(this.stone_breadth, this.stone_depth, this.stone_length, 0, 0, 0);
+        this.stone = new THREE.Mesh(this.stone_geometry, this.stone_material);
+
+        this.cylinder1_radius = 1
+        this.cylinder1_height = 20
+        this.cylinder1_texture = new THREE.TextureLoader().load('./textures/gold.jpg');
+        this.cylinder1_material = new THREE.MeshBasicMaterial({map: this.cylinder1_texture});
+        this.cylinder1_geometry = new THREE.CylinderGeometry(this.cylinder1_radius, this.cylinder1_radius, this.cylinder1_height, 40, 40);
+        this.cylinder1 = new THREE.Mesh(this.cylinder1_geometry, this.cylinder1_material);
+
+        this.cylinder2_radius = 1
+        this.cylinder2_height = 20
+        this.cylinder2_texture = new THREE.TextureLoader().load('./textures/gold.jpg');
+        this.cylinder2_material = new THREE.MeshBasicMaterial({map: this.cylinder2_texture});
+        this.cylinder2_geometry = new THREE.CylinderGeometry(this.cylinder2_radius, this.cylinder2_radius, this.cylinder2_height, 40, 40);
+        this.cylinder2 = new THREE.Mesh(this.cylinder2_geometry, this.cylinder2_material);
+
+        this.cylinder3_radius = 1
+        this.cylinder3_height = 20
+        this.cylinder3_texture = new THREE.TextureLoader().load('./textures/gold.jpg');
+        this.cylinder3_material = new THREE.MeshBasicMaterial({map: this.cylinder3_texture});
+        this.cylinder3_geometry = new THREE.CylinderGeometry(this.cylinder3_radius, this.cylinder3_radius, this.cylinder3_height, 40, 40);
+        this.cylinder3 = new THREE.Mesh(this.cylinder3_geometry, this.cylinder3_material);
+
+        this.cylinder4_radius = 1
+        this.cylinder4_height = 20
+        this.cylinder4_texture = new THREE.TextureLoader().load('./textures/gold.jpg');
+        this.cylinder4_material = new THREE.MeshBasicMaterial({map: this.cylinder4_texture});
+        this.cylinder4_geometry = new THREE.CylinderGeometry(this.cylinder4_radius, this.cylinder4_radius, this.cylinder4_height, 40, 40);
+        this.cylinder4 = new THREE.Mesh(this.cylinder4_geometry, this.cylinder4_material);
+
+        
+
+        this.setPosOri();
+        this.addDependencies();
+    }
+
+    setPosOri()
+    {
+      this.stone.position.set(0,0,0)
+      this.stone.scale.set(0.25, 0.25, 0.25)
+      this.cylinder1.rotation.z = Math.PI/2
+      this.cylinder2.rotation.z = Math.PI/2
+      this.cylinder3.rotation.z = Math.PI/2
+      this.cylinder4.rotation.z = Math.PI/2
+
+      this.cylinder2.rotation.y = Math.PI/4
+      this.cylinder3.rotation.y = -1*Math.PI/4
+      this.cylinder4.rotation.y = Math.PI/2
+    }
+
+    addDependencies()
+    {
+        const AxesHelper = new THREE.AxesHelper();
+        this.entity.add(this.stone)
+        this.stone.add(this.cylinder1)
+        this.stone.add(this.cylinder2)
+        this.stone.add(this.cylinder3)
+        this.stone.add(this.cylinder4)
+
+    }
+
+}
+
+var nr = new NoseRing();
+var nose_ring = nr.entity;
 
 
 //add the nose ring to the scene
@@ -81,8 +150,10 @@ spinner.ontransitionend = () => {
   spinner.style.display = 'none';
 };
 
+var min_val = 10000000000
+var max_val = -100000000000
 function onResultsFaceMesh(results) {
-
+  flag = 0
   //console.log(results.multiFaceLandmarks[0]) //Printing the 468 face mesh landmarks
 
   document.body.classList.add('loaded');
@@ -91,38 +162,62 @@ function onResultsFaceMesh(results) {
   /*Position the nose ring appropriately for each frame*/
   var width = visibleWidthAtZDepth(0, cam)
   var height = visibleHeightAtZDepth(0, cam)
-  // console.log("width: "+width)
-  // console.log("height: "+height)
-  // console.log("window_width: "+window.innerWidth)
-  // console.log("window_height: "+window.innerHeight)
-  
+
   var x_avg = 0;
   var y_avg = 0;
+  var z_avg = 0;
 
   landmarks = [281, 363, 440, 420, 456]
   for(let i=0;i<landmarks.length;i++)
   {
     x_avg += results.multiFaceLandmarks[0][landmarks[i]].x;
     y_avg += results.multiFaceLandmarks[0][landmarks[i]].y;
+    z_avg += results.multiFaceLandmarks[0][landmarks[i]].z;
   }
   x_avg /= landmarks.length
   y_avg /= landmarks.length
+  z_avg /= landmarks.length
 
   nose_ring.position.x = (x_avg - 0.5)*width*(-1)
   nose_ring.position.y = (y_avg - 0.5)*height*(-1)
-  nose_ring.position.z = results.multiFaceLandmarks[0][0].z
-
-
-  // console.log("x: "+nose_ring.position.x)
-  // console.log("y: "+nose_ring.position.y)
-  // console.log("z: "+nose_ring.position.z)
+  nose_ring.position.z = z_avg
+  if(nose_ring.position.z >= max_val){
+    max_val = nose_ring.position.z
+  }
+  if(nose_ring.position.z <= min_val){
+    min_val = nose_ring.position.z
+  }
 
 
   /*Scale the nose ring appropriately for each frame ...TODO */
-  
+  scale_value = 10*((-0.0062) - nose_ring.position.z)/((-0.0062) - (-0.1593)) + 1
+  nose_ring.scale.set(scale_value, scale_value, scale_value)
 
   /*Orient the nose ring appropriately for each frame ...TODO */
+  l1 = results.multiFaceLandmarks[0][4]
+  l2 = results.multiFaceLandmarks[0][1]
+  l3 = results.multiFaceLandmarks[0][45]
 
+  v1 = new THREE.Vector3(0, 0, 0)
+  v2 = new THREE.Vector3(0, 0, 0)
+
+  v1.x = l2.x - l1.x
+  v1.y = l2.y - l1.y
+  v1.z = l2.z - l1.z
+  v2.x = l3.x - l1.x
+  v2.y = l3.y - l1.y
+  v2.z = l3.z - l1.z
+
+  cross_v = new THREE.Vector3(0, 0, 0)
+  cross_v.crossVectors(v1, v2)
+  len_cross_v = cross_v.length()
+  cross_v.x = cross_v.x/len_cross_v
+  cross_v.y = cross_v.y/len_cross_v
+  cross_v.z = cross_v.z/len_cross_v
+
+  nose_ring.rotation.x = Math.acos(cross_v.x) 
+  nose_ring.rotation.y = Math.acos(cross_v.y)
+  nose_ring.rotation.z = Math.acos(cross_v.z)
 
 }
 
@@ -173,5 +268,3 @@ new ControlPanel(controlsElement2, {
       video2.classList.toggle('selfie', options.selfieMode);
       faceMesh.setOptions(options);
     });
-
-
